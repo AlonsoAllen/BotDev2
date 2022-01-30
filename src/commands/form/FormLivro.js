504a36ecf2b8afa5f639d97cd7af2cd851f30cb3
Cassano <ninocb@gmail.com>
2022-01-30 18:46:39 -0300

    Remoção linha 95
    .save() no lugar errado.

--- src/commands/form/FormLivro.js
+++ src/commands/form/FormLivro.js
@@ -93,7 +93,7 @@ module.exports = class extends Command {
                 console.log(answers)
             }
             return answers
-            .save()        
+                 
         }
     }
 }
